53cd3a55610e129951209492013f855cc084a243
aikawatakuya <85001312+aikawatakuya@users.noreply.github.com>
2021-09-26 15:12:23 +0900

    モバイルスクロール　画像変更php

--- archive.php
+++ archive.php
@@ -61,6 +61,10 @@
       <!-- /.nav-btn -->
     </header>
     <!-- /#header -->
+    <div class="c-top-view">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/home/ocean_2.jpg" alt="" />
+      </div>
+      <!-- /.c-top-view -->
     <section class="news">
       <div class="c-container">
         <h3 class="c-title02  clip-js2 left">
